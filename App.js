8f39b09f0da9d86381ec2d7477b3bbb136bb24f3
gptsuraj <gptsuraj96@gmail.com>
2024-08-16 04:29:53 +0530

    push1

---
+++ App.js
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View, Appearance } from 'react-native'
+import React, { useCallback, useEffect, useState } from 'react'
+import Router from './Router'
+
+const App = () => {
+  return (
+    <Router />
+  )
+}
+export default App
